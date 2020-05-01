0f854db722e422dd867d7a973b5de9a6ad7675fe
Goldie698 <63108245+Goldie698@users.noreply.github.com>
2020-05-01 16:27:04 +0100

    Amended to deploy

--- pubquiz/settings.py
+++ pubquiz/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+# Added to deploy on Heroku
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -119,8 +122,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'pubquiz/static/')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Amended for deployment on Heroku
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Added for deployment.
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
