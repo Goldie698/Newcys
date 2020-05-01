f8352ea0ccd113a88b3f23f4c0352ed6e4d5753e
Goldie698 <63108245+Goldie698@users.noreply.github.com>
2020-05-01 16:28:02 +0100

    Amended for deployment

--- pubquiz/wsgi.py
+++ pubquiz/wsgi.py
@@ -5,12 +5,14 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+
+This file has been amended to make it suitable for deployment on Heroku.
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pubquiz.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
 
-application = get_wsgi_application()
+application = DjangoWhiteNoise(get_wsgi_application())
